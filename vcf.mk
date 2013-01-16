e19cbe48a96844e67f04b5483e32fd46414b1207
James Gleeson <james@innovativemedicine.ca>
2013-01-16 10:56:26 -0500

    more messing around with makefiles to make it possible to 'include' it in other makefiles

--- vcf.mk
+++ vcf.mk
@@ -1,14 +1,17 @@
-ROOT := .
-SCRIPTS := $(ROOT)/script
-TESTS := $(ROOT)/test
-3RDPARTY_SCRIPTS := $(ROOT)/3rdparty/script
+# You can include this file in your project's makefile like so:
+#
+# VPATH = path/to/pyvcf
+# include path/to/pyvcf/vcf.mk
+# something.py: src/vcf/vcfparser.py
+
 PYTHON := python
+YAPPS := 3rdparty/script/yapps2.py
 
 export PYTHON ROOT
 
 all: src/vcf/vcfparser.py
 
-src/vcf/vcfparser.py: src/vcf/vcfparser.g
-	$(3RDPARTY_SCRIPTS)/yapps2.py $<
+src/vcf/vcfparser.py: src/vcf/vcfparser.g $(YAPPS)
+	$(word 2,$^) $<
 	$(eval VCFPARSER := $$(patsubst %.g,%.py,$$<))
 	chmod +x $(VCFPARSER)
